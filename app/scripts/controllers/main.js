dfd4d432e79ff260050401a25990575f53718f56
alenoir <antoine@yupeek.com>
2014-08-12 17:58:47 +0200

    init app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name giftabApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the giftabApp
+ */
+angular.module('giftabApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    var token = 'b0cdb95cca00131';
+    $scope.imgTitle = '';
+    $scope.imgSrc = '/images/loader.gif';
+
+    $http({method: 'GET', url: 'https://api.imgur.com/3/gallery/r/reactiongifs', headers: {'Authorization': 'Client-ID ' + token}}).
+      success(function(result) {
+        var items = result.data;
+        var item = items[Math.floor(Math.random()*items.length)];
+
+        $scope.imgTitle = item.title;
+        $scope.imgSrc = item.link;
+    });
+  });
